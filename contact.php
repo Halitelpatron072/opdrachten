f99d1756fd13cc23431085b4caba36fb938e804e
Halit <183967@student.talland.nl>
2025-10-10 15:18:00 +0200

    dsds

--- contact.php
+++ contact.php
@@ -34,21 +34,14 @@
                 </div>
             </header>
             <main class="row mt-4">
-                <form>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                <form action="" method="GET">
+                    <label for="fname">First Name: </label><br>
+                    <input type="text" id="fname" name="fname"><br>
+                    <label for="lname">Last name: </label><br>
+                    <input type="text" id="lname" name="lname"><br>
+                    <label for="email">Email: </label><br>
+                    <input type="text" id="email" name="email"><br><br>
+                    <input type="submit" value="Submit"> 
                 </form>
             </main>
         </div>
